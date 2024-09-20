18b7bd5dabe0663e7c8249eab5f4556edd29c665
Gautam <gautamy499@gmail.com>
2024-09-20 16:38:09 +0530

    add functions in componenet

--- main.js
+++ main.js
@@ -1,13 +1,87 @@
-const cube = document.getElementById('defaultBox');
-const box1 = document.getElementById('box-1');
+AFRAME.registerComponent('markerhandler', {
+    init: function () {
+        const animatedMarker = document.querySelector("#markercube");
+        const aEntity = document.querySelector("#box-1");
 
-cube.addEventListener('markerFound', () => {
-    console.log('Marker found');
-    box1.object3D.rotation.x += 0.1;
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function (ev, target) {
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            console.log('click');
+        });
+    }
+});
+const markercube = document.getElementById('markercube');
+const box = document.getElementById('box-1');
+let boxMove = false;
+
+markercube.addEventListener('markerFound', () => {
+    boxMove = true;
+    setInterval(cubemoving, 200);
 });
 
-cube.addEventListener('markerLost', () => {
-    console.log('Marker lost');
-    box1.object3D.rotation.x = 0;
+function cubemoving() {
+    if (boxMove) {
+        box.rotation.x += 0.1;
+    }
+    else {
+        box.rotation.x = 0;
+    }
+}
+
+markercube.addEventListener('markerLost', () => {
+    boxMove = false;
+    clearInterval(moving);
+    markercube.removeEventListener('markerFound', cubemoving);
+    markercube.removeEventListener('markerLost', cubemoving);
+});
+
+const markerball = document.getElementById('markerball');
+const fireball = document.getElementById('fireball-1');
+let fireballMove = false;
+
+markerball.addEventListener('markerFound', () => {
+    fireballMove = true;
+    setInterval(ballmoving, 200);
+});
+
+function ballmoving() {
+    if (fireballMove) {
+        fireball.rotation.x += 0.1;
+    }
+    else {
+        fireball.rotation.x = 0;
+    }
+}
+
+markerball.addEventListener('markerLost', () => {
+    fireballMove = false;
+    clearInterval(ballmoving);
+    markerball.removeEventListener('markerFound', ballmoving);
+    markerball.removeEventListener('markerLost', ballmoving);
+});
+
+const markermag = document.getElementById('markermag');
+const mag = document.getElementById('magnemite');
+let magmove = false;
+
+markermag.addEventListener('markerFound', () => {
+    magmove = true;
+    setInterval(magmoving, 200);
+});
+
+function magmoving() {
+    if (magmove) {
+        mag.rotation.x += 0.1;
+    }
+    else {
+        mag.rotation.x = 0;
+    }
+}
+
+markermag.addEventListener('markerLost', () => {
+    magmove = false;
+    clearInterval(magmoving);
+    markermag.removeEventListener('markerFound', magmoving);
+    markermag.removeEventListener('markerLost', magmoving);
 });
 
