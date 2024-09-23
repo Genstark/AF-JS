b89f8885ad2c6e63fab5f2c1456b2beea6e1ad50
Gautam <gautamy499@gmail.com>
2024-09-23 11:05:29 +0530

    add funciton in model

---
+++ hiro.js
@@ -0,0 +1,22 @@
+const marker1 = document.getElementById('markercube');
+const model1 = document.getElementById('model-11');
+let ismodel1 = false;
+
+marker1.addEventListener('markerFound', () => {
+    ismodel1 = true;
+    setInterval(moving(ismodel1), 100);
+});
+
+function moving(box){
+    if(box){
+        model1.ariaOrientation.x += 0.1;
+    }
+    else{
+        model1.ariaOrientation.x = 0;
+    }
+}
+
+marker1.addEventListener('markerLost', () => {
+    ismodel1 = false;
+    clearInterval(moving(ismodel1));
+});
