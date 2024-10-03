9654b1e20f5e650d74a439a11c7974c4689df53d
Gautam <gautamy499@gmail.com>
2024-10-03 13:11:42 +0530

    remove 2nd model and modify index file

--- frame data/src/components/Model.vue
+++
@@ -1,9 +0,0 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<template>
-    <div></div>
-</template>
-
-<script setup>
-import 'aframe';
-import 'ar.js/aframe/build/aframe-ar.js';
-</script>
