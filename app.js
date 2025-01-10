086a503169f59320d98992bdc0112e40b2b8c942
Gautam <gautamy499@gmail.com>
2025-01-10 16:56:38 +0530

    add server

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname, 'public'), { 'extensions': ['html', 'css', 'js', 'glb'] }));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 app.listen(1000, () => {
     console.log(`Server is running on http://localhost:${1000}`);
